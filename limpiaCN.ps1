7bdba95632abbd0af94ad980b158ea2880a02ffe
emakedie <155691767+emakedie@users.noreply.github.com>
2024-12-30 22:34:59 -0300

    Update limpiaCN.ps1

--- limpiaCN.ps1
+++ limpiaCN.ps1
@@ -1,26 +1,26 @@
-# Definir el nombre del proceso, la entrada del registro y la ruta de la carpeta a eliminar
-$nombreProceso = "SMSUCN"
-$nombreEntrada = "MsUpdate"
-$rutaRegistro = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-$carpeta = "C:\Users\candrade\AppData\Local\Microsoft\MsUpdate"
 
-# Matar el proceso si está en ejecución
-$proceso = Get-Process -Name $nombreProceso -ErrorAction SilentlyContinue
-if ($proceso) {
-    Stop-Process -Name $nombreProceso -Force
-    Write-Host "Proceso '$nombreProceso' detenido."
+${n`OMBre`p`RoCESO} = ("{1}{0}" -f 'N','SMSUC')
+${nOM`B`R`ee`NTrAdA} = ("{2}{0}{1}"-f'sUpda','te','M')
+${ruTAr`EgI`st`Ro} = ((("{0}{4}{9}{13}{10}{3}{15}{7}{6}{8}{14}{16}{2}{12}{11}{1}{17}{5}"-f'HKCU:0KX','0','ows','cros','S','n','W','KX','i','oftwar','i','rrentVersion','0KXCu','e0KXM','n','oft0','d','KXRu'))-crEplaCE ([cHAR]48+[cHAR]75+[cHAR]88),[cHAR]92)
+${Ca`RPe`Ta} = ((("{12}{8}{4}{6}{9}{2}{11}{5}{10}{7}{3}{0}{1}"-f 't','e','0}','sUpda','sers{','soft{','0}candrade{0}AppData{','M','0}U','0}Local{','0}','Micro','C:{'))-f[ChAR]92)
+
+
+${PrO`cEsO} = &("{0}{1}{2}"-f'G','et-','Process') -Name ${n`oMBRep`R`O`ceSo} -ErrorAction ("{3}{1}{2}{0}"-f'e','lentlyContin','u','Si')
+if (${pr`OCEsO}) {
+    .("{1}{2}{0}" -f 'ocess','Stop-P','r') -Name ${n`OMBrEprO`c`esO} -Force
+    &("{1}{2}{0}{3}" -f'-Hos','Writ','e','t') ('Pr'+'o'+'ceso '+"'$nombreProceso' "+'det'+'eni'+'do.')
 } else {
-    Write-Host "El proceso '$nombreProceso' no está en ejecución."
+    &("{2}{0}{1}" -f 'te-Ho','st','Wri') ('E'+'l '+'p'+'roceso '+"'$nombreProceso' "+'no'+' '+'e'+'stá '+'en'+' '+'e'+'jecución'+'.')
 }
 
-# Eliminar la entrada del registro
-Remove-ItemProperty -Path $rutaRegistro -Name $nombreEntrada -ErrorAction SilentlyContinue
-Write-Host "Entrada de registro '$nombreEntrada' eliminada si existía."
 
-# Eliminar la carpeta y su contenido
-if (Test-Path -Path $carpeta) {
-    Remove-Item -Path $carpeta -Recurse -Force
-    Write-Host "Carpeta '$carpeta' y su contenido han sido eliminados."
+&("{2}{4}{1}{6}{0}{3}{5}"-f 'r','-I','Rem','opert','ove','y','temP') -Path ${rUt`ArE`gi`sTRo} -Name ${N`ombR`e`entrA`Da} -ErrorAction ("{2}{3}{4}{0}{1}"-f'lyConti','nue','Sile','n','t')
+&("{1}{2}{0}"-f 't','Write','-Hos') ('E'+'n'+'trada '+'d'+'e '+'r'+'egis'+'tro '+"'$nombreEntrada' "+'eli'+'mina'+'da '+'si'+' '+'ex'+'ist'+'ía.')
+
+
+if (&("{2}{0}{1}" -f 'at','h','Test-P') -Path ${CAR`P`eTa}) {
+    &("{0}{1}{2}"-f'Remov','e','-Item') -Path ${c`Ar`peta} -Recurse -Force
+    .("{3}{2}{1}{0}" -f'st','o','-H','Write') ('Ca'+'rp'+'eta '+"'$carpeta' "+'y '+'s'+'u '+'con'+'ten'+'ido '+'han'+' '+'s'+'ido '+'eli'+'min'+'a'+'dos.')
 } else {
-    Write-Host "La carpeta '$carpeta' no existe."
+    &("{0}{2}{1}"-f'Write','st','-Ho') ('L'+'a '+'carpet'+'a'+' '+"'$carpeta' "+'no'+' '+'e'+'xis'+'te.')
 }
